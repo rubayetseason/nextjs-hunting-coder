0bd87a2fcb902ef7fdd70e81062137326f8c13f3
rubayet-islam <season.ss2001@gmail.com>
2023-01-01 15:13:36 +0600

    added JSON folders for blogpost data

--- pages/blog.js
+++ pages/blog.js
@@ -1,27 +1,28 @@
-import React from 'react';
-import styles from '../styles/Blog.module.css';
-import Link from 'next/link';
+import React from "react";
+import styles from "../styles/Blog.module.css";
+import Link from "next/link";
 
 const Blog = () => {
-    return (
-        <div className={styles.container}>
-        <main className={styles.main}>
-            <div>
-            <Link href={'/blogposts/learn-javascript'}>
-                    <h3>How to learn JavaScript in 2022?</h3></Link>
-                <p>JavaScript is the language used to design logic for the web</p>
-            </div>
-            <div className="blogItem">
-                <h3 className={styles.blogName}>How to learn JavaScript in 2022?</h3>
-                <p>JavaScript is the language used to design logic for the web</p>
-            </div>
-            <div className="blogItem">
-                <h3 className={styles.blogName}>How to learn JavaScript in 2022?</h3>
-                <p>JavaScript is the language used to design logic for the web</p>
-            </div>
-        </main>
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <div>
+          <Link href={"/blogposts/learn-javascript"}>
+            <h2>How to learn JavaScript in 2022?</h2>
+          </Link>
+          <p>JavaScript is the language used to design logic for the web</p>
+        </div>
+        <div className="blogItem">
+          <h2 className={styles.blogName}>How to learn JavaScript in 2022?</h2>
+          <p>JavaScript is the language used to design logic for the web</p>
+        </div>
+        <div className="blogItem">
+          <h2 className={styles.blogName}>How to learn JavaScript in 2022?</h2>
+          <p>JavaScript is the language used to design logic for the web</p>
+        </div>
+      </main>
     </div>
-    );
+  );
 };
 
 export default Blog;
