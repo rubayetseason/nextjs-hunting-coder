298764dc26fe7de2cf10ccbde008e5b9cd4d93a7
rubayet-islam <season.ss2001@gmail.com>
2023-01-01 14:57:22 +0600

    blogpost page created

--- pages/blog.js
+++ pages/blog.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import styles from '../styles/Blog.module.css';
+import Link from 'next/link';
 
 const Blog = () => {
     return (
-        <div>
-           this is blogs page 
-        </div>
+        <div className={styles.container}>
+        <main className={styles.main}>
+            <div>
+            <Link href={'/blogposts/learn-javascript'}>
+                    <h3>How to learn JavaScript in 2022?</h3></Link>
+                <p>JavaScript is the language used to design logic for the web</p>
+            </div>
+            <div className="blogItem">
+                <h3 className={styles.blogName}>How to learn JavaScript in 2022?</h3>
+                <p>JavaScript is the language used to design logic for the web</p>
+            </div>
+            <div className="blogItem">
+                <h3 className={styles.blogName}>How to learn JavaScript in 2022?</h3>
+                <p>JavaScript is the language used to design logic for the web</p>
+            </div>
+        </main>
+    </div>
     );
 };
 
